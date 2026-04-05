499da90f3d5e8221fef289e4822b74cb51e15357
ajaycsharp <40476595+ajaycsharp@users.noreply.github.com>
2026-04-04 20:48:49 -0400

    Add files via upload
    Session 7 (10th March) - Data types and variables

---
+++ T1 - Variable.py
@@ -0,0 +1,45 @@
+age = 25  
+print(age)
+name ="Alice"
+print(name)
+price = 19.99
+print(price)
+
+is_active = True
+print(is_active)
+
+x = 10
+print(x)
+
+a = 5
+b = 3
+
+result = a + b
+print(result)
+
+score = 50
+print(score)
+
+score = 100
+print(score)
+
+first_name = "John"
+last_name = "Doe"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+length = 10
+width = 5
+area = length + width
+print(area)
+
+x = 10
+print(x)
+
+x = 20
+print(x)
+
+
+
+
